623394dfc95c253a5e143091b19b0bb3102ac103
johny payeras <yohpayeras@gmail.com>
2025-09-08 16:05:35 +0000

    trip._fact.sql

---
+++ models/trip_fact.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+WITH trip AS (
+    SELECT 
+        RIDE_ID,
+        RIDEABLE_TYPE,
+        TO_TIMESTAMP(STARTED_AT) AS TRIP_DATE,
+        START_STATION_ID,
+        END_STATION_ID,
+        MEMBER_CASUAL,
+        TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(STARTED_AT), TO_TIMESTAMP(ENDED_AT)) AS  TRIP_DURATION_SECONDS
+        
+    FROM {{ source('source_demo', 'bike') }}
+    WHERE RIDE_ID != 'ride_id'
+)
+
+SELECT * FROM trip
